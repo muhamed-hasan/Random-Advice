3f5f7b3a1ad4d554b5973d918a79aa9ddc1ca25e
Mohamed Mostafa <muhamedmustafa3.14@gmail.com>
2022-06-02 00:54:02 +0200

    random activity model

---
+++ lib/feature/random_activity/data/models/random_activity_model.dart
@@ -0,0 +1,32 @@
+import '../../domain/entities/random_activity.dart';
+
+class RandomActivityModel extends RandomActivity {
+  RandomActivityModel(
+      {required super.activity,
+      required super.type,
+      required super.link,
+      required super.participants,
+      required super.price,
+      required super.accessibility});
+
+  factory RandomActivityModel.fromJson(Map<String, dynamic> json) {
+    return RandomActivityModel(
+        activity: json['activity'],
+        type: json['type'],
+        link: json['link'],
+        participants: json['participants'],
+        price: json['price'],
+        accessibility: json['accessibility']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'activity': activity,
+      'type': type,
+      'link': link,
+      'participants': participants,
+      'price': price,
+      'accessibility': accessibility
+    };
+  }
+}
