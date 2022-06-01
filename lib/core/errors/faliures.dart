c252bdb7e1dd126edaae8fae689577112cb3ee41
Mohamed Mostafa <muhamedmustafa3.14@gmail.com>
2022-06-02 00:12:56 +0200

    failure class

---
+++ lib/core/errors/faliures.dart
@@ -0,0 +1,14 @@
+abstract class Failure {
+  final String errorMessage;
+  Failure({
+    required this.errorMessage,
+  });
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.errorMessage});
+}
+
+class CacheFailure extends Failure {
+  CacheFailure({required super.errorMessage});
+}
